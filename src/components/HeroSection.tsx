7c455794a0e5bf84833ef97763a58b8b24ba4d6e
Arsh <66792944+Warfarian@users.noreply.github.com>
2025-06-14 17:51:26 +0530

    Move scroll indicator lower on mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -60,7 +60,7 @@ const HeroSection: React.FC = () => {
       </div>
       
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-16 sm:bottom-8 left-1/2 transform -translate-x-1/2">
         <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-white/50 rounded-full mt-2 animate-pulse"></div>
         </div>
